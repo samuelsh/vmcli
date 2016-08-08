fe87ceb806f2b7356ee96e6d40cda25ef511aaf5
samuelsh <samuel.sh79@gmail.com>
2016-08-08 12:18:22 +0300

    vm_tool: adding views

--- vmutils.py
+++ vmutils.py
@@ -84,8 +84,8 @@ class VmUtils(object):
     @staticmethod
     def print_folder(folder, level=0):
         try:
-            print("{0} {1}".format('-' * level, folder.name))
             if folder.childEntity:
+                print("{0} {1}".format('-' * level, folder.name))
                 VmUtils.print_folder(folder.childEntity[0], level + 1)
         except AttributeError:
             pass
