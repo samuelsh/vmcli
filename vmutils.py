f35990c5a419d86c80ae428bb18f29ea1be9b750
samuelsh <samuel.sh79@gmail.com>
2016-08-09 15:30:07 +0300

    vm_tool: adding views

--- vmutils.py
+++ vmutils.py
@@ -101,10 +101,7 @@ class VmUtils(object):
                         tree_start_char = TREE_LEAF
                     print("{0}{1}{2} {3} ({4} of {5})".format(' ' * level, tree_start_char, TREE_LEVEL, f.name, i,
                                                               len(folder.childEntity) - 1))
-                if not hasattr(f, 'capability') and f.childEntity:
-                    tree_start_char = TREE_LEAF_END
-                    print("{0}{1}{2} {3} ({4} of {5})".format(' ' * level, tree_start_char, TREE_LEVEL, f.name, i,
-                                                              len(folder.childEntity) - 1))
+                if f.childEntity:
                     VmUtils.print_folder(f, level + 1)  # go deeper it's a folder
         except AttributeError:
             pass
