1dc39abd7a0bfbeb71a0312fd3c7fdfbd1fc4375
samuelsh <samuel.sh79@gmail.com>
2016-08-14 12:31:54 +0300

    vm_tool: adding vmtree util to trint Datacenter structure in tree view

--- vmutils.py
+++ vmutils.py
@@ -97,14 +97,13 @@ class VmUtils(object):
             if folders_only:
                 child_folders = [f for f in child_folders if not hasattr(f, 'capability')]  # removing VMs from list
             for i, f in enumerate(child_folders):
-                if not hasattr(f, 'capability'):  # checking if entity isn't a VM
-                    if i >= len(child_folders) - 1:
-                        tree_start_char = TREE_LEAF_END
-                    else:
-                        tree_start_char = TREE_LEAF
-                    print("{0}{1}{2} {3} ({4} of {5})".format(' ' * level, tree_start_char, TREE_LEVEL, f.name, i,
+                if i >= len(child_folders) - 1:
+                    tree_start_char = TREE_LEAF_END
+                else:
+                    tree_start_char = TREE_LEAF
+                print("{0}{1}{2} {3} ({4} of {5})".format(' ' * level, tree_start_char, TREE_LEVEL, f.name, i,
                                                               len(child_folders) - 1))
-                elif f.childEntity:
+                if f.childEntity:
                     VmUtils.print_folder(f, level + 1)  # go deeper it's a folder
         except AttributeError as att_err:
             print(att_err)
