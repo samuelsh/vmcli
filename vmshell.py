eafb44addc9f613793764b62d88b67c4d2c915cf
samuelsh <samuel.sh79@gmail.com>
2016-08-31 08:04:04 +0300

    vm_tool: adding vmtree util to trint Datacenter structure in tree view

--- vmshell.py
+++ vmshell.py
@@ -13,9 +13,14 @@ class VMShell(cmd.Cmd, object):
         self.si = si
         self.args = args
         self.file = None
-        self.current_path = "/"
+        self.current_displayed_path = "/"
+        self.current_real_path = None
         self.hostname = args.host
-        self._my_prompt = '[{0}@{1} {2}]#'.format(args.user, self.hostname, self.current_path)
+        self._my_prompt = '[{0}@{1} {2}]# '.format(args.user, self.hostname, self.current_displayed_path)
+
+        self.content = si.RetrieveContent()
+        #for child in content.rootFolder.childEntity:
+
 
     @property
     def my_prompt(self):
@@ -24,7 +29,14 @@ class VMShell(cmd.Cmd, object):
     prompt = my_prompt
 
     def do_pwd(self, arg):
-        print("{0}".format(self.current_path))
+        print("{0}".format(self.current_displayed_path))
+
+    def do_ls(self, arg):
+        try:
+            print("{0}".format(self.content.rootFolder.name))
+        except Exception:
+            pass
+
 
     def do_bye(self, arg):
         """Stop recording, close the turtle window, and exit:  BYE"""
